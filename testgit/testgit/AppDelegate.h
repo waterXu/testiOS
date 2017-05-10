01fb5f3b8c188e7cb5e8cc9e8b409d1d15b79e0a
jiangziwei <jiangziwei7@qq.com>
2017-05-10 15:58:55 +0800

    创建工程

---
+++ testgit/testgit/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  testgit
+//
+//  Created by jiangziwei on 2017/5/10.
+//  Copyright © 2017年 jiangziwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ testgit/testgit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testgit
+//
+//  Created by jiangziwei on 2017/5/10.
+//  Copyright © 2017年 jiangziwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
